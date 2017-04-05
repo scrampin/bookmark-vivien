cb8dd02ac1bfb065f759fb6225acd5f6705f5e16
Stephanie Crampin <steph.crampin@gmail.com>
2017-04-05 11:57:01 +0100

    added heroku capability

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,6 +4,6 @@ require 'dm-postgres-adapter'
 require_relative 'models/tag'
 require_relative 'models/link'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
