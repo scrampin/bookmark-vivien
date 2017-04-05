915da2cbbda449c47b75bd9e74a935d10b28b9f8
Magnus Holm <mail@magnusholm.com>
2017-04-05 12:37:56 +0100

    add filter-by-tag feature and new page in controller

--- app/app.rb
+++ app/app.rb
@@ -24,5 +24,10 @@ ENV["RACK_ENV"] ||= "development"
     redirect('/links')
   end
 
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links =  (tag ? tag.links : [])
+    erb :'links/index'
+  end
 
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,5 +5,6 @@ class Tag
   property :id, Serial
   property :name, String
 
+  has n, :links, through: Resource
 
 end
